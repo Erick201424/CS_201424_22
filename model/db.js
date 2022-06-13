75c965be64bb32b2ec940342efcc07aab8cbd422
ErickDiaz <201424@ids.upchiapas.edu.mx>
2022-06-12 19:23:08 -0500

    Resolving Connect DB Postgres - Heroku

--- model/db.js
+++ model/db.js
@@ -1,38 +1,38 @@
-import  pkg  from 'pg';
+import pkg from 'pg';
 import { Sequelize } from 'sequelize';
 import { db } from '../config.js';
 const { Pool } = pkg
 
 async function getConnection() {
-    const client = new Pool({
-      user: db.user,
-      host: db.host,
-      database: db.database,
-      password: db.password,
-      port: db.port,
-    });
-    await client.connect();
-    return client;
+  const client = new Pool({
+    user: db.user,
+    host: db.host,
+    database: db.database,
+    password: db.password,
+    port: db.port,
+  });
+  await client.connect();
+  return client;
 }
 
+
 const sequelizeClient = new Sequelize(db.database, db.user, db.password, {
-    dialectOptions:{
-        ssl: {
-            require: true,
-            rejectUnauthorized:false
-        }
+  host: db.host,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
     }
-},{
-    host: db.host,
-    dialect: 'postgres',
+  }
 });
 
 sequelizeClient.authenticate()
-  .then(() =>{
+  .then(() => {
     console.log('Conectado')
   })
-  .catch(() =>{
+  .catch(() => {
     console.log('No se conecto')
   });
 
-export const getData = {getConnection, sequelizeClient};
+export const getData = { getConnection, sequelizeClient };
