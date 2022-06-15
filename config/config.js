f0d4577b6ae64280273b4b3917e55df9c92c7b95
ErickDiaz <201424@ids.upchiapas.edu.mx>
2022-06-14 23:10:11 -0500

    Change project

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 export const api = {
     port: process.env.API_PORT || 3000,
 };
+
 export const db = {
     user: "ywyoexjkwwbddn",
     host: "ec2-34-225-159-178.compute-1.amazonaws.com",
@@ -8,3 +9,11 @@ export const db = {
     password: "b2e303f3a4add12d87158f15f922649a5991561025e583b87ca00c8f11689c99",
     port: "5432",
 };
+
+// export const db = {
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'CSDB',
+//     password: 'root',
+//     port: '5432',
+// };
---
+++ models/db.js
@@ -0,0 +1,23 @@
+import { Sequelize } from 'sequelize';
+import { db } from '../config/config.js';
+
+const sequelizeClient = new Sequelize(db.database, db.user, db.password, {
+  dialectOptions: {
+       ssl: {
+           require: true,
+           rejectUnauthorized: false
+       }
+  },
+  host: db.host,
+  dialect: 'postgres',
+});
+
+sequelizeClient.sync({ force: true })
+    .then(() => {
+        console.log('Conectado')
+    })
+    .catch((err) => {
+        console.log('No se conecto', err)
+    });
+
+export const getData = { sequelizeClient };
